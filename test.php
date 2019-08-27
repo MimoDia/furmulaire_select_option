79aef5a38638bc634d452ce43e3c23579d4d1492
ibrahimasow <ibrahima.sow@ausy.fr>
2019-08-27 17:22:51 +0200

    add file test

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 <?php
+
+/*
 echo '<select name="source" id="source" multiple="multiple">';
 
 $data = array('dakar', 'tamba', 'kidira');
@@ -7,17 +9,17 @@ foreach($data as $key => $value){
 
 	$beSelected = $source==$value ? ' selected="selected"' : '';
 
-	echo '<option value="'.$value.'"'.$beSelected.'>' .$value. '</option>';
+	echo '<option value="'.$value.'" '.$beSelected.' >' .$value. '</option>';
 
 
 }
 
 echo '</select>';
-?>
 
 
 
-<?php
+
+
 echo '<select name="source" id="source" multiple="multiple">';
 
 $data = array('dakar', 'tamba', 'kidira');
@@ -33,12 +35,12 @@ foreach($data as $key => $value){
 }
 
 echo '</select>';
-?>
 
 
 
-<?php
 
+
+ 
 $data = [
 	'dakar' => 'Dakar',
 	'tamba' => 'Tamba',
@@ -57,10 +59,84 @@ foreach($data as $key => $value){
 }
 
 echo '</select>';
-?>
 
-<!--
+*/
+/*<!--
 	<option value="ohjfoojojif" select="selected" > toto </option>;
 	<option value="dakar" select="dakar" > dakar </option>;
--->
+-->*/
+
+
+
+class Utilisateur{
+
+	// attribut
+	private $nom;
+	private $prenom;
+	private $naissance;
+	private $pays;
+	private $date_creation_compte;
+
+	// constructeur
+	//public function __construct($nom, $prenom, $pays, $naissance, $date_creation_compte){
+	public function __construct($uid = NULL){
+		if ($uid) {
+			// requete en BDD
+			$retourBdd = [
+				'id' => 123,
+				'nom' => 'Dia',
+				'prenom' => 'Babacar',
+				'naissance' => 'Mali',
+				'pays' => 'fr',
+				'date_creation_compte' => '2010-05-30 16:16:16',
+			];
+
+			// et set les propriétés :
+			$this->nom= $retourBdd['nom'];
+			$this->prenom= $retourBdd['prenom'];
+			$this->naissance= $retourBdd['naissance'];
+			$this->pays= $retourBdd['pays'];
+			$this->date_creation_compte= $retourBdd['date_creation_compte'];
+		}
+	}
+		// methode
+	private function propertyExists($property) {
+		if (!property_exists($this, $property)) {
+			echo 'caca';
+			// ou return FALSE;
+		}
+	}
+
+	public function set($property, $value){
+		$this->propertyExists($property);
+		$this->$property = $value;
+		return $this;// ou pas !
+	}
+	public function get($property){
+		$this->propertyExists($property);
+		return $this->$property;
+	}
+
+
+
+
+
+	//Methode de transformation de l'objet de nom et prenom en chaine de caractere
+	public function __toString(){
+		return "Bonjour {$this->prenom}  {$this->nom}" ;
+	}
+
+}
+
+
+$user = new Utilisateur(1);
+
+var_dump($user); 
+
+
+
+$user->set('prenom', 'Ibrahima');
+echo($user); // Affiche Bonjour Ibrama Dia
+
+echo '</br>';
 
